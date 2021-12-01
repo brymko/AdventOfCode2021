daafe97e95d9e92e5a0c6f0381b0653b3b854234
brymko <47984664+brymko@users.noreply.github.com>
2021-12-01 09:35:59 +0100

    update

--- day1.ml
+++ day1.ml
@@ -1,37 +1,25 @@
-type foldexpr = { counter: int; last : int option}
-
 let int_of_bool b = if b then 1 else 0
-
-let do_fold expr cur =
-    match expr.last with
-    | None -> {expr with last = Some cur }
-    | Some last -> {counter = expr.counter + int_of_bool (cur > last); last = Some cur}
-
-let (>>) f g x = g (f x)
+let sum = List.fold_left (+) 0
+let (>>) g f x = f (g x)
 
 let rec map_slide3 f = function
     | a :: b :: c :: r -> f [a; b; c;] :: map_slide3 f (b :: c :: r)
     | _ -> []
 
-let sum = List.fold_left (+) 0
-
-let grab_counter x = x.counter
+let rec fold_window2 f y = function
+    | a :: b :: r -> fold_window2 f (f y a b) (b :: r)
+    | _ -> y
 
 let main () = 
     let chall = Core.In_channel.read_all "day1.input" |> String.split_on_char '\n' |> List.filter_map (String.trim >> int_of_string_opt) in
 
-    chall
-    |> List.fold_left do_fold { counter = 0; last = None }
-    |> grab_counter
+    chall 
+    |> fold_window2 (fun ctr a b -> ctr + int_of_bool (a < b)) 0
     |> string_of_int
     |> print_endline;
 
     chall
     |> map_slide3 sum 
-    |> List.fold_left do_fold { counter = 0; last = None }
-    |> grab_counter
+    |> fold_window2 (fun ctr a b -> ctr + int_of_bool (a < b)) 0
     |> string_of_int
     |> print_endline
-
-
-
