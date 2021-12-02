02d9a1bf659e36d116a2f8bf4d43a6bc3ca78a65
brymko <47984664+brymko@users.noreply.github.com>
2021-12-02 10:13:22 +0100

    cleanup

--- day2.ml
+++ day2.ml
@@ -1,35 +1,24 @@
-type command =
-    | Forward of int
-    | Down of int
-    | Up of int
-
-let command_of_string_list = function
-    | a :: b :: _ -> (match a with
-        | "forward" -> Some (Forward (int_of_string b))
-        | "down" -> Some (Down (int_of_string b))
-        | "up" -> Some (Up (int_of_string b))
-        | _ -> None)
-    | _ -> None
-
-let convert line = 
-    line |> String.split_on_char ' ' |> List.map String.trim |> command_of_string_list
-
+let (>>) g f x = 
+    f (g x)
 
 let main () = 
-    let chall = Core.In_channel.read_all "day2.input" |> String.split_on_char '\n' |> List.map String.trim |> List.filter_map convert in 
+    let chall = Core.In_channel.read_all "day2.input" |> String.split_on_char '\n' |> List.map (String.trim >> String.split_on_char ' ') in
 
-    List.fold_left (fun (hpos, depth) e -> match e with
-        | Forward i -> (hpos + i, depth)
-        | Down i -> (hpos, depth + i)
-        | Up i -> (hpos, depth - i)
-    ) (0, 0) chall
+    List.fold_left (fun (hpos, depth) line -> match (List.hd line, List.(rev line |> hd) |> int_of_string_opt) with
+        | ("forward", Some i) -> (hpos + i, depth)
+        | ("down", Some i) -> (hpos, depth + i)
+        | ("up", Some i) -> (hpos, depth - i)
+        | _ -> (hpos, depth)
+    ) (0, 0)  chall
     |> (fun (a, b) -> a * b) |> string_of_int |> print_endline;
 
-    List.fold_left (fun (hpos, depth, aim) e -> match e with
-        | Forward i -> (hpos + i, depth + aim * i, aim)
-        | Down i -> (hpos, depth, aim + i)
-        | Up i -> (hpos, depth, aim - i)
-    ) (0, 0, 0) chall
-    |> (fun (a, b, _) -> a * b) |> string_of_int |> prerr_endline
+    
+    List.fold_left (fun (hpos, depth, aim) line -> match (List.hd line, List.(rev line |> hd) |> int_of_string_opt) with
+        | ("forward", Some i) -> (hpos + i, depth + aim * i, aim)
+        | ("down", Some i) -> (hpos, depth, aim + i)
+        | ("up", Some i) -> (hpos, depth, aim - i)
+        | _ -> (hpos, depth, aim)
+    ) (0, 0, 0)  chall
+    |> (fun (a, b, _) -> a * b) |> string_of_int |> print_endline
 
     
